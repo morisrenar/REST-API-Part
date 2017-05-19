f9b0c5bfad62aa3549e20f50efaeedc86eef4a25
bmshamsnahid <morisrenar@gmail.com>
2017-05-19 10:13:08 +0600

    part 2: database model

---
+++ app/models/contacts/index.js
@@ -0,0 +1,11 @@
+var mongoose  = require('mongoose');
+mongoose.Promise = global.Promise;
+
+var Schema = mongoose.Schema;
+
+var contactSchema = new Schema({
+    personName: String,
+    personNumber: String
+});
+
+module.exports = mongoose.Model('Contact', contactSchema);
--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 var db = {
-    db: 'mainDatabsaeAddress',
-    dbTest: 'testDatabaseAddress',
+    db: 'localhost:27017/contactsBook',
+    dbTest: 'localhost:27017/contactsBookTest',
     mySecret: 'Eminem'
 };
 
